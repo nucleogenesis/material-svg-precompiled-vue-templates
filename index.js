bed160b71b8a58120432e5b2c137734068a3b083
Jacob Pierce <w.jacobpierce@gmail.com>
2020-02-26 11:19:38 -0800

    Cleanup & commenting

--- index.js
+++ index.js
@@ -9,49 +9,30 @@ const styles = ":style=style";
 const a11yAttrs = `role="presentation" focusable="false"`;
 const cssClass = `:class="rtlClass"`;
 
+// Make the base svg directory
 fs.mkdirSync('./svg');
 
 fs.readdirSync(materialSvgPath).forEach(svgDir => {
+    // This is the ./svg/NAME_OF_ICON directory
     const localDir = `./svg/${svgDir}`;
     fs.mkdirSync(localDir);
     
     const materialDir = `${materialSvgPath}/${svgDir}`;
+    // Read the SVGs from the material icons package directory
     fs.readdirSync(materialDir).forEach(svgFile => {
+        // Read the SVG
         const materialSvgFile = fs.readFileSync(`${materialDir}/${svgFile}`, 'utf8');
+        // Apply the Kolibri-specific a11y styles and attrs
         const styledAndAccessibleSvg = materialSvgFile.replace("<svg", `<svg ${cssClass} ${styles} ${a11yAttrs}`);
-        
-
+        // Generate the template functions, an object with a render key that returns a fn
         const template = VueTemplateCompiler.compileToFunctions(styledAndAccessibleSvg);
+        // Parse the text of that fn into its AST
         const ast = espree.parse(template.render.toString(), { sourceType: 'script', ecmaVersion: 2015 });
+        // Generate the file itself with module.exports for easy require()ing
         const exportText = `module.exports = ${escodegen.generate(ast)}`.replace('anonymous', 'render');
+        // Write that file as a js file in the same place it and filename (sans .svg) it uses in
+        // the material-icons package.
         const newFileName = svgFile.replace('svg', 'js');
         fs.writeFileSync(`${localDir}/${newFileName}`, exportText);
     });
 });
-
-/*
-fs.readdir(materialSvgPath, (err, svgDirs) => {
-    svgDirs.forEach(dir => {
-        // Create the package dir.
-        const svgDir = `./svg/${dir}`;
-        fs.mkdirSync(svgDir);
-
-        // Compile each template and write it to local dir
-        const currentMaterialSvgPath = `${materialSvgPath}/${dir}`;
-        fs.readdir(currentMaterialSvgPath, (err, currentSvgs) => {            
-            currentSvgs.forEach(svgFile => {
-                const svg = fs.readFileSync(`${currentMaterialSvgPath}/${svgFile}`);
-                console.log(svg);
-                const svgBase64 = svg.replace(svgFilePrefix, "");
-                const svgElementString = atob(svgBase64);
-                const styledAndAccessibleSvg = svgElementString.replace("<svg", `<svg ${cssClass} ${styles} ${a11yAttrs}`);
-
-                const template = VueTemplateCompiler.compileToFunctions(styledAndAccessibleSvg);
-                const newFileName = svgFile.replace('svg', 'js');
-                fs.writeFileSync(`${svgDir}/${newFileName}`, template);
-            })
-        })
-    })
-});
-
-*/
